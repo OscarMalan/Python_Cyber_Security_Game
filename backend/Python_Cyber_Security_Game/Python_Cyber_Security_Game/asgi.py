f5165422afaeeff2bfc6632dfe3ced9f812cbc1a
Oscar Malan <96668033+OscarMalan@users.noreply.github.com>
2023-02-13 21:04:22 +0000

    Developing a mock of a django backend Developing a mock of a django backend, to see if I could make one work, as it would allow for better interaction with the GPIO headers on the RPi

---
+++ backend/Python_Cyber_Security_Game/Python_Cyber_Security_Game/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Python_Cyber_Security_Game project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Python_Cyber_Security_Game.settings')
+
+application = get_asgi_application()
