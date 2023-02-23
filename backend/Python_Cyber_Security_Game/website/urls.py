7a8e98529b59730abdfa249f4da0502abfe33e02
Oscar Malan <96668033+OscarMalan@users.noreply.github.com>
2023-02-23 21:29:27 +0000

    Fix a bug with JS and improve coding grammer Had the JavaScript file in index.html incorrectly named, so fixed that. Also improved some grammer in my code by using double quotation marks instead of single for strings

--- backend/Python_Cyber_Security_Game/website/urls.py
+++ backend/Python_Cyber_Security_Game/website/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Handles which page you go to based on the URL
+    # Runs a function from the views file based on which URL you are at (ie "localhost:8000/quiz")
     path("", views.home),
     path("quiz/", views.quiz),
     path("wrong/", views.wrong),
--- backend/Python_Cyber_Security_Game/website/views.py
+++ backend/Python_Cyber_Security_Game/website/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/index.html')
+    return render(request, "home/index.html")
 
 def quiz(request):
-    return render(request, 'quiz/quiz.html')
+    return render(request, "quiz/quiz.html")
 
 def wrong(request):
     # Place code for the GPIO headers here
     print("wrong")
-    return render(request, 'wrong/wrong.html')
+    return render(request, "wrong/wrong.html")
 
 def right(request):
     # Place code for the GPIO headers here
     print("right")
-    return render(request, 'right/right.html')
+    return render(request, "right/right.html")
